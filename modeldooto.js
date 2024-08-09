eda9c68059b69edc8e327d56e6b166a1f7035801
thiagoRamos26 <devisonthiago26@gmail.com>
2024-08-09 18:05:15 -0300

    Add files via upload

--- modeldooto.js
+++ modeldooto.js
@@ -3,7 +3,7 @@ function clicar() {
     var v = window.document.getElementById('caixa02')
     a.innerHTML = 'CLIK'
     a.style.background = 'red'
-    v.style.background = 'transparent'
+    v.style.display = 'none'
     v.style.border = '1px solid white'
     v.style.color = 'transparent'
 }
